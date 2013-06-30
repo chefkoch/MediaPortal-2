c5c5e75be5ff43465a91b8d8c3be83a597b44f78
chefkoch <chefkoch@team-mediaportal.com>
2013-06-30 19:11:43 +0200

    added UpdateVersion.targets to write git hash to various files containing version information

--- MediaPortal/Build/MSBUILD_Create_Full_Installer.bat
+++ MediaPortal/Build/MSBUILD_Create_Full_Installer.bat
@@ -3,10 +3,13 @@ echo Deleting binary folder
 rmdir /s /q ..\bin
 
 echo Restore NuGet packages
-call RestorePackages.bat
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" RestorePackages.targets
 
 echo Updating language resources from transifex
 call TRANSIFEX_Update_Translation_Files.bat
+
+echo Writing version information to VersionInfo.cs (will be used for AssemblyVersion information)...
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" UpdateVersion.targets
 
 echo Rebuilding Server
 call MSBUILD_Rebuild_Release_Server.bat
