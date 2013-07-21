242992c3f03955f018d691f13971b7d146494270
chefkoch <chefkoch@team-mediaportal.com>
2013-07-21 19:47:32 +0200

    reworked MP2 build script into MSBuild project

--- MediaPortal/Build/MSBUILD_Rebuild_Release_Server.bat
+++ MediaPortal/Build/MSBUILD_Rebuild_Release_Server.bat
@@ -1,10 +1,11 @@
 
+call RestorePackages.bat
 xcopy /I /Y .\BuildReport\_BuildReport_Files .\_BuildReport_Files
 
 set xml=Build_Report_Release_Server.xml
 set html=Build_Report_Release_Server.html
 
 set logger=/l:XmlFileLogger,"BuildReport\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" ..\Source\MP2-Server.sln %logger% /target:Rebuild /property:Configuration=Release;Platform=x86
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" Build.msbuild %logger% /property:OneStepOnly=true;BuildServer=true
 
 BuildReport\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
