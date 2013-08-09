ba372e6f65a844cafab1992060a4681e714cc044
chefkoch <chefkoch@team-mediaportal.com>
2013-08-09 12:31:00 +0200

    use BaseClasses submodule and added sample build script

---
+++ MediaPortal/Source/UI/Players/EVRPresenter/build.cmd
@@ -0,0 +1,3 @@
+
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /t:Rebuild /p:Configuration=Debug;PlatformToolset=Windows7.1SDK source/EVRPresenter.vcxproj
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /t:Rebuild /p:Configuration=Release;PlatformToolset=Windows7.1SDK source/EVRPresenter.vcxproj
